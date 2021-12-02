cf0ae73b610d8caa6b67df827be39977aaa0fb81
Duncan Nielsen <duncann008@gmail.com>
2021-12-02 10:59:45 -0600

    back in class

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const menuList = (state = [], action) =>    {
+    if (action.type === 'GET_MENU') {
+        console.log(action);
+        return action.payload;
+    }
+    return state;
+}
+
+const reduxStore = createStore(
+    combineReducers({
+        menuList
+    }),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(<Provider store={reduxStore}><App /></Provider>, document.getElementById('root'));
